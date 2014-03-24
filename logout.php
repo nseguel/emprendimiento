298316f4d172210bac982cee1f2f6fc4cf73beab
Nicolas Seguel <nseguel@tucadis.cl>
2014-03-24 02:31:14 -0400

    actualiza
    actualixa

--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php require_once '../config.php';
-        // connecting to mysql
-        $con = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
-        // selecting database
-        mysql_select_db(DB_DATABASE);
-        session_destroy(); ?>
